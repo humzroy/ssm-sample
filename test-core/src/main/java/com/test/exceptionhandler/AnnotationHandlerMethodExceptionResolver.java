f79a7324ee552930cf98f09191cd3dbb60dc5ccc
wuhengzhen <wuhengzhen_qd@caxins.com>
2018-08-08 16:38:02 +0800

    mybatis自定义插件

--- test-core/src/main/java/com/test/exceptionhandler/AnnotationHandlerMethodExceptionResolver.java
+++ test-core/src/main/java/com/test/exceptionhandler/AnnotationHandlerMethodExceptionResolver.java
@@ -1,13 +1,15 @@
-package com.test.exceptionhandler; /**
- * 软件著作权：长安新生（深圳）金融投资有限公司
- * 
- * 系统名称：马达贷
- * 
- */
-
+package com.test.exceptionhandler;
 
 import com.test.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.HandlerMethod;
@@ -15,117 +17,163 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Properties;
-import java.util.Set;
+
 
 /**
- *
- *
- * @author 郑翔
- */
-public class AnnotationHandlerMethodExceptionResolver extends ExceptionHandlerExceptionResolver{
+ * 不必在Controller中对异常进行处理，抛出即可，由此异常解析器统一控制。<br>
+ * ajax请求（有@ResponseBody的Controller）发生错误，输出JSON。<br>
+ * 页面请求（无@ResponseBody的Controller）发生错误，输出错误页面。<br>
+ * 需要与AnnotationMethodHandlerAdapter使用同一个messageConverters<br>
+ * Controller中需要有专门处理异常的方法。
+ **/
+public class AnnotationHandlerMethodExceptionResolver extends ExceptionHandlerExceptionResolver {
 
-	//private static Logger logger = LoggerFactory.getLogger(AnnotationHandlerMethodExceptionResolver.class);
-	  private String defaultErrorView;
-	  private Properties exceptionMappings;
+    private static Logger logger = LoggerFactory.getLogger(AnnotationHandlerMethodExceptionResolver.class);
 
-	  public String getDefaultErrorView()
-	  {
-	    return this.defaultErrorView;
-	  }
+    private String defaultErrorView;
+    private Properties exceptionMappings;
 
-	  public void setDefaultErrorView(String defaultErrorView) {
-	    this.defaultErrorView = defaultErrorView;
-	  }
+    public String getDefaultErrorView() {
+        return this.defaultErrorView;
+    }
 
-	  protected ModelAndView doResolveHandlerMethodException(HttpServletRequest request, HttpServletResponse response, HandlerMethod handlerMethod, Exception exception)
-	  {
-		 
-		  exception.printStackTrace();
-		  
-	    if (handlerMethod == null) {
-	      return null;
-	    }
+    public void setDefaultErrorView(String defaultErrorView) {
+        this.defaultErrorView = defaultErrorView;
+    }
 
-	    Method method = handlerMethod.getMethod();
+    public Properties getExceptionMappings() {
+        return this.exceptionMappings;
+    }
 
-	    if (method == null) {
-	      return null;
-	    }
+    public void setExceptionMappings(Properties exceptionMappings) {
+        this.exceptionMappings = exceptionMappings;
+    }
 
-	    ModelAndView returnValue = super.doResolveHandlerMethodException(request, response, handlerMethod, exception);
+    /**
+     * 异常后跳转到页面
+     *
+     * @param request
+     * @param response
+     * @param handlerMethod
+     * @param exception
+     * @return
+     */
+    @Override
+    protected ModelAndView doResolveHandlerMethodException(HttpServletRequest request, HttpServletResponse response, HandlerMethod handlerMethod, Exception exception) {
 
-	    ResponseBody responseBodyAnn = (ResponseBody)AnnotationUtils.findAnnotation(method, ResponseBody.class);
-	    if (("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) || (responseBodyAnn != null)) {
-	      try
-	      {
-	        ModelAndView jsonView = new ModelAndView(new MappingJackson2JsonView());
+        exception.printStackTrace();
 
-	        if ((exception instanceof BusinessException)) {
-	         // logger.error("PanCloud捕获BusinessException:" + exception.getMessage());
-	          jsonView.addObject(exception.getMessage());
-	        } else {
-	         // logger.error("PanCloud捕获非BusinessException:" + exception.getMessage());
-	          jsonView.addObject("系统异常！");
-	        }
-	        response.setStatus(500);
-	        return jsonView;
-	      }
-	      catch (Exception e) {
-	        return null;
-	      }
-	    }
-	    if (this.exceptionMappings != null) {
-	      Set<Entry<Object,Object>> eSet = this.exceptionMappings.entrySet();
-	      for (Entry entry : eSet) {
-	        if (exception.getClass().getName().equals(entry.getKey())) {
-	          Map param = new HashMap();
-	          String errorMsg = exception.getMessage();
-	          
-	          if (StringUtils.isEmpty(errorMsg)) {
-	            errorMsg = "系统异常！";
-	          }
-	          try {
-				param.put("errorMsg", URLEncoder.encode(errorMsg,"UTF-8"));
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}
-	          //logger.error("PanCloud捕获" + entry.getKey() + ":" + errorMsg);
-	          returnValue = new ModelAndView(entry.getValue().toString(), param);
-	          break;
-	        }
-	      }
-	    }
+        if (handlerMethod == null) {
+            return null;
+        }
 
-	    if (returnValue == null) {
-	      Map param = new HashMap();
-	      try {
-			param.put("errorMsg",   URLEncoder.encode("系统异常！","UTF-8"));
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	      response.reset();
-	      returnValue = new ModelAndView(this.defaultErrorView, param);
-	    }
+        Method method = handlerMethod.getMethod();
 
-	    return returnValue;
-	  }
+        if (method == null) {
+            return null;
+        }
+        // 如果定义了ExceptionHandler则返回相应的Map中的数据
+        ModelAndView returnValue = super.doResolveHandlerMethodException(request, response, handlerMethod, exception);
 
-	  public Properties getExceptionMappings()
-	  {
-	    return this.exceptionMappings;
-	  }
+        ResponseBody responseBodyAnn = (ResponseBody) AnnotationUtils.findAnnotation(method, ResponseBody.class);
+        if (("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) || (responseBodyAnn != null)) {
+            try {
+                ModelAndView jsonView = new ModelAndView(new MappingJackson2JsonView());
 
-	  public void setExceptionMappings(Properties exceptionMappings) {
-	    this.exceptionMappings = exceptionMappings;
-	  }
-	
+                if ((exception instanceof BusinessException)) {
+                    logger.error("PanCloud捕获BusinessException:" + exception.getMessage());
+                    jsonView.addObject(exception.getMessage());
+                } else {
+                    logger.error("PanCloud捕获非BusinessException:" + exception.getMessage());
+                    jsonView.addObject("系统异常！");
+                }
+                response.setStatus(500);
+                return jsonView;
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        if (this.exceptionMappings != null) {
+            Set<Entry<Object, Object>> eSet = this.exceptionMappings.entrySet();
+            for (Entry entry : eSet) {
+                if (exception.getClass().getName().equals(entry.getKey())) {
+                    Map param = new HashMap();
+                    String errorMsg = exception.getMessage();
+
+                    if (StringUtils.isEmpty(errorMsg)) {
+                        errorMsg = "系统异常！";
+                    }
+                    try {
+                        param.put("errorMsg", URLEncoder.encode(errorMsg, "UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    logger.error("PanCloud捕获" + entry.getKey() + ":" + errorMsg);
+                    returnValue = new ModelAndView(entry.getValue().toString(), param);
+                    break;
+                }
+            }
+        }
+
+        if (returnValue == null) {
+            Map param = new HashMap();
+            try {
+                param.put("errorMsg", URLEncoder.encode("系统异常！", "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            response.reset();
+            returnValue = new ModelAndView(this.defaultErrorView, param);
+        }
+
+        return returnValue;
+    }
+
+    /**
+     * 异常后 返回json
+     *
+     * @param returnValue
+     * @param request
+     * @param response
+     * @return
+     * @throws ServletException
+     * @throws IOException
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private ModelAndView handleResponseBody(ModelAndView returnValue, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map value = returnValue.getModelMap();
+        HttpInputMessage inputMessage = new ServletServerHttpRequest(request);
+        List<MediaType> acceptedMediaTypes = inputMessage.getHeaders().getAccept();
+        if (acceptedMediaTypes.isEmpty()) {
+            acceptedMediaTypes = Collections.singletonList(MediaType.ALL);
+        }
+        MediaType.sortByQualityValue(acceptedMediaTypes);
+        HttpOutputMessage outputMessage = new ServletServerHttpResponse(response);
+        Class<?> returnValueType = value.getClass();
+        List<HttpMessageConverter<?>> messageConverters = super.getMessageConverters();
+        if (messageConverters != null) {
+            for (MediaType acceptedMediaType : acceptedMediaTypes) {
+                for (HttpMessageConverter messageConverter : messageConverters) {
+                    if (messageConverter.canWrite(returnValueType, acceptedMediaType)) {
+                        messageConverter.write(value, acceptedMediaType, outputMessage);
+                        return new ModelAndView();
+                    }
+                }
+            }
+        }
+        if (logger.isWarnEnabled()) {
+            logger.warn("Could not find HttpMessageConverter that supports return type [" + returnValueType + "] and " + acceptedMediaTypes);
+        }
+        return null;
+    }
+
 }
---
+++ test-plugins/src/main/java/com/test/plugins/mybatis/generator/CommentGenerator.java
@@ -0,0 +1,28 @@
+package com.test.plugins.mybatis.generator;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+/**
+ * @author : wuhengzhen
+ * <p>
+ * 扩展一个 mybatis generator 插件，用于不仅仅在生成的实体类 还有 *Example 类都序列化
+ * @Description :生成model中，字段增加注释
+ * @date : 2018/08/08 13:59
+ * @system name:
+ * Copyright 2018 <a href="https://github.com/wuhengzhen" target="_blank">https://github.com/wuhengzhen</a>. All rights reserved.
+ */
+public class CommentGenerator extends DefaultCommentGenerator {
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        super.addFieldComment(field, introspectedTable, introspectedColumn);
+        if (introspectedColumn.getRemarks() != null && !"".equals(introspectedColumn.getRemarks())) {
+            field.addJavaDocLine("/**");
+            field.addJavaDocLine(" * " + introspectedColumn.getRemarks());
+            addJavadocTag(field, false);
+            field.addJavaDocLine(" */");
+        }
+    }
+}

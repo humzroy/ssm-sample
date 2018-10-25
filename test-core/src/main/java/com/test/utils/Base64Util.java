1ebf9a3189d3ab750ad98e05da3de9409c30cb33
wuhengzhen <wuhengzhen_qd@caxins.com>
2018-10-25 14:29:46 +0800

    图片和Base64互相转换

--- test-core/src/main/java/com/test/utils/Base64Util.java
+++ test-core/src/main/java/com/test/utils/Base64Util.java
@@ -1,10 +1,13 @@
 package com.test.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
-import static com.test.utils.KaisaCodeUtil.decodeKaisa;
 
-import java.security.NoSuchAlgorithmException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Description：BASE64 加密工具类
@@ -13,6 +16,11 @@ import java.security.NoSuchAlgorithmException;
  * Time：11:50
  */
 public class Base64Util {
+    /**
+     * logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(Base64Util.class);
+
     /**
      * BASE64解密
      *
@@ -35,8 +43,113 @@ public class Base64Util {
         return (new BASE64Encoder()).encodeBuffer(key);
     }
 
+
+    /**
+     * 本地图片转换成base64字符串
+     *
+     * @param imgFile 图片本地路径
+     * @return
+     * @author ZHANGJL
+     * @dateTime 2018-02-23 14:40:46
+     */
+    public static String imageToBase64byLocal(String imgFile) throws Exception {
+        InputStream in = null;
+        byte[] data = null;
+        // 读取图片字节数组
+        try {
+            in = new FileInputStream(imgFile);
+            data = new byte[in.available()];
+            in.read(data);
+        } catch (IOException e) {
+            logger.error(ExceptionUtil.getStackTrace(e));
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+        return encryptBASE64(data);
+    }
+
+
+    /**
+     * 在线图片转换成base64字符串
+     *
+     * @param imgURL 图片线上路径
+     * @return
+     * @author ZHANGJL
+     * @dateTime 2018-02-23 14:43:18
+     */
+    public static String imageToBase64ByOnline(String imgURL) throws Exception {
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        InputStream is = null;
+        try {
+            // 创建URL
+            URL url = new URL(imgURL);
+            byte[] by = new byte[1024];
+            // 创建链接
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);
+            is = conn.getInputStream();
+            // 将内容读取内存中
+            int len = -1;
+            while ((len = is.read(by)) != -1) {
+                data.write(by, 0, len);
+            }
+        } catch (IOException e) {
+            logger.error(ExceptionUtil.getStackTrace(e));
+        } finally {
+            if (is != null) {
+                // 关闭流
+                is.close();
+            }
+        }
+        return encryptBASE64(data.toByteArray());
+    }
+
+
+    /**
+     * base64字符串转换成图片
+     *
+     * @param imgStr      base64字符串
+     * @param imgFilePath 图片存放路径
+     * @return
+     * @author ZHANGJL
+     * @dateTime 2018-02-23 14:42:17
+     */
+    public static boolean base64ToImage(String imgStr, String imgFilePath) throws IOException {
+        // 对字节数组字符串进行Base64解码并生成图片
+        OutputStream out = null;
+        // 图像数据为空
+        if (StringUtils.isEmpty(imgStr)) {
+            return false;
+        }
+        try {
+            // Base64解码
+            byte[] b = decryptBASE64(imgStr);
+            for (int i = 0; i < b.length; ++i) {
+                // 调整异常数据
+                if (b[i] < 0) {
+                    b[i] += 256;
+                }
+            }
+            out = new FileOutputStream(imgFilePath);
+            out.write(b);
+            out.flush();
+            return true;
+        } catch (Exception e) {
+            logger.error(ExceptionUtil.getStackTrace(e));
+            return false;
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        String clearStr = "12yyyy34567890-=rtyuiodsfsdfsdfklmsfmsdflmsdafsdfsdfsdfwerrwerpteoryym/,ppdksdfldfsdlkmmsdf6783$%#!~!#$%^&*&(WUHENGZHENwuhengzhen22411";
+        /*String clearStr = "12yyyy34567890-=rtyuiodsfsdfsdfklmsfmsdflmsdafsdfsdfsdfwerrwerpteoryym/,ppdksdfldfsdlkmmsdf6783$%#!~!#$%^&*&(WUHENGZHENwuhengzhen22411";
         String clearMd5 = null;
         try {
             clearMd5 = MD5Utils.md5Hex(clearStr);
@@ -52,6 +165,25 @@ public class Base64Util {
         System.out.println("原文：" + clearStr);
         System.out.println("md5：" + clearMd5);
         System.out.println("base64：" + cipherStr.trim());
-        System.out.println("base64长度：" + cipherStr.trim().length());
+        System.out.println("base64长度：" + cipherStr.trim().length());*/
+
+        //本地图片地址
+        String url = "E:/img/38.jpg";
+        //在线图片地址
+        String string = "http://bpic.588ku.com//element_origin_min_pic/17/03/03/7bf4480888f35addcf2ce942701c728a.jpg";
+        String str;
+        try {
+            str = imageToBase64byLocal(url);
+            String ste = imageToBase64ByOnline(string);
+            System.out.println(str);
+
+            base64ToImage(str, "E:/img/test1.jpg");
+
+            base64ToImage(ste, "E:/img/test2.jpg");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
